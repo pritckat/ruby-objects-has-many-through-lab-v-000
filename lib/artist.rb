7dbbbc0b5f91549d37dee1001e52f89b1560ee4b
Katrina Pritchard <pritckat@gmail.com>
2018-08-22 15:26:43 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,7 +14,7 @@ class Artist
   end
 
   def new_song(name, genre)
-    song = Song.new
+    song = Song.new(name, self, genre)
     song.artist = self
     song.genre = genre
     song.name = name
